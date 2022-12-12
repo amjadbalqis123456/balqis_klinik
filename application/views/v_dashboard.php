16f2ea53b0b0e50a494d8a15565f7e57cdf8eb49
Ekafitriani11 <ekkafitrianii890@gmail.com>
2022-12-12 11:12:36 +0700

    unggah ke github

---
+++ application/views/v_dashboard.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <div class="jumbotron">
+        <h1 class="display-4">KLINIK BALQIS</h1>
+        <p class="lead">JANGAN LUPA TERSENYUMM</p>
+        <hr class="my-4">
+    </div>
+    <br><br>
+    <!-- <img src="<?= base_url('assets/orang.gif') ?>" alt=""> -->
+</div>
